3f1c09bb7a946e6492d5e50bdb3a89c1d33046fb
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-15 15:33:17 +0900

    understand Entity

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // nullable: null 허용 여부
@@ -17,4 +17,28 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
